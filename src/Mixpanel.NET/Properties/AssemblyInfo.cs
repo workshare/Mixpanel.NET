39080142bbe7d303f62949f885dfdce380c0b00e
pg75 <philip.grobler@workshare.com>
2018-07-13 15:10:48 +0100

    Use Global Assembly Info

--- src/Mixpanel.NET/Properties/AssemblyInfo.cs
+++ src/Mixpanel.NET/Properties/AssemblyInfo.cs
@@ -7,12 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Mixpanel.NET")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Mixpanel.NET")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
